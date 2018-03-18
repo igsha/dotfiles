6836ccefdb44d99180fd58cb78f696a3c627a352
Igor Sharonov <igsha@users.noreply.github.com>
2018-03-19 00:04:41 +0300

    Try to fix keyindicator

--- templates/keyindicator.sh
+++ templates/keyindicator.sh
@@ -6,5 +6,5 @@ xset -q | awk '
         if($4 == "on") caps = "#00FF00";
         if($8 == "on") num = "#00FF00";
         if($12 == "on") scrl="#00FF00";
-        print "<span color="caps">CAPS</span> <span color="num">NUM</span> <span color="scrl">SRL</span>"
+        printf "<span color=\"%s\">CAPS</span> <span color=\"%s\">NUM</span> <span color=\"%s\">SRL</span>", caps, num, scrl
     }'
