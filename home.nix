ba3a948cb912017f01a12b936b35b69d4eb27ad1
Igor Sharonov <igsha@users.noreply.github.com>
2018-07-08 13:10:24 +0300

    Separates git config

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, email ? "igsha@users.noreply.github.com" }:
+{ pkgs, user, email }:
 
 {
   home.packages = [
@@ -7,37 +7,7 @@
 
   programs = {
     home-manager.enable = true;
-    git = {
-      enable = true;
-      userName  = "Igor Sharonov";
-      userEmail = email;
-      aliases = {
-        st = "status";
-        di = "difftool";
-        br = "branch";
-        co = "checkout";
-        lg = "log --oneline";
-        sm = "submodule";
-        ci = "commit";
-        graph = "log --graph --oneline --all";
-      };
-      extraConfig = {
-        core.editor = "nvim";
-        color = {
-          status = "auto";
-          branch = "auto";
-          diff = "auto";
-        };
-        diff.tool = "nvimdiff2";
-        "difftool \"nvimdiff2\"".cmd = "nvim -d $LOCAL $REMOTE -c '$wincmd w'";
-        difftool.prompt = false;
-        merge.tool = "nvimdiff3";
-        "mergetool \"nvimdiff3\"".cmd = "nvim -d $BASE $LOCAL $REMOTE $MERGED -c '$wincmd w' -c 'wincmd J'";
-        push.default = "current";
-        pull.rebase = true;
-        credential.helper = "cache --timeout=3600";
-      };
-    };
+    git = import ./gitConfig.nix { userName = user; userEmail = email; };
     fzf.enable = true;
     pidgin = {
       enable = true;
