27a8062949929c781648bbc31b7a364b8812a300
Igor Sharonov <igsha@users.noreply.github.com>
2018-10-11 22:12:20 +0300

    Adds tmux plugins

--- configuration.nix
+++ configuration.nix
@@ -135,7 +135,15 @@
     terminal = "screen-256color";
     shortcut = "a";
     extraTmuxConf = ''
-      set -g mouse on
+      run-shell ${pkgs.tmuxPlugins.prefix-highlight}/share/tmux-plugins/prefix-highlight/prefix_highlight.tmux
+      run-shell ${pkgs.tmuxPlugins.sidebar}/share/tmux-plugins/sidebar/sidebar.tmux
+      run-shell ${pkgs.tmuxPlugins.urlview}/share/tmux-plugins/urlview/urlview.tmux
+      run-shell ${pkgs.tmuxPlugins.yank}/share/tmux-plugins/yank/yank.tmux
+      run-shell ${pkgs.tmuxPlugins.pain-control}/share/tmux-plugins/pain-control/pain_control.tmux
+      run-shell ${pkgs.tmuxPlugins.logging}/share/tmux-plugins/logging/logging.tmux
+      run-shell ${pkgs.tmuxPlugins.open}/share/tmux-plugins/open/open.tmux
+      run-shell ${pkgs.tmuxPlugins.copycat}/share/tmux-plugins/copycat/copycat.tmux
+      ${builtins.readFile ./configs/tmux.conf}
     '';
   };
 
