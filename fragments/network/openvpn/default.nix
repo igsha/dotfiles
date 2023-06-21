83d7dc9211391682977c63a49bc8795287095962
Igor Sharonov <igsha@users.noreply.github.com>
2023-06-21 20:02:21 +0300

    Update openvpn

--- fragments/network/openvpn/default.nix
+++ fragments/network/openvpn/default.nix
@@ -2,4 +2,22 @@ _:
 
 {
   services.openvpn.restartAfterSleep = false;
+
+  nixpkgs.overlays = [
+    (self: super: {
+      openvpn-systemd-resolved-up-script = ''
+        for var in ''${!foreign_option_*}; do
+          x=(''${!var})
+          if [ "''${x[0]}" = dhcp-option ]; then
+            if [ "''${x[1]}" = DOMAIN ]; then domains+=" ''${x[2]}"
+            elif [ "''${x[1]}" = DNS ]; then nameservers+=" ''${x[2]}"
+            fi
+          fi
+        done
+
+        ${super.systemd}/bin/resolvectl dns $dev ''${nameservers[@]}
+        ${super.systemd}/bin/resolvectl domain $dev ''${domains[@]}
+      '';
+    })
+  ];
 }
