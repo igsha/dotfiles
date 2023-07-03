e492abf1080524fd01ecc9486290e85bfa2d0f30
Igor Sharonov <igsha@users.noreply.github.com>
2023-07-03 17:04:35 +0300

    tor: use snowflake bridge

--- fragments/network/tor/default.nix
+++ fragments/network/tor/default.nix
@@ -1,10 +1,6 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
-let
-  torList = builtins.fetchurl https://torscan-ru.ntc.party/relays.txt;
-  servers = builtins.filter (x: x != "") (lib.strings.splitString "\n" (builtins.readFile torList));
-
-in {
+{
   services.tor = {
     enable = true;
     client = {
@@ -12,7 +8,27 @@ in {
       dns.enable = false;
     };
     settings = {
-      FascistFirewall = true;
-    } // builtins.listToAttrs (map (x: lib.nameValuePair "Bridge" x) servers);
+      Bridge = lib.strings.concatStringsSep " " [
+        "snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA"
+        "fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA"
+        "url=https://snowflake-broker.torproject.net.global.prod.fastly.net/"
+        "front=cdn.sstatic.net "
+        ("ice=" + lib.strings.concatStringsSep "," [
+          "stun:stun.l.google.com:19302"
+          "stun:stun.antisip.com:3478"
+          "stun:stun.bluesip.net:3478"
+          "stun:stun.dus.net:3478"
+          "stun:stun.epygi.com:3478"
+          "stun:stun.sonetel.net:3478"
+          "stun:stun.uls.co.za:3478"
+          "stun:stun.voipgate.com:3478"
+          "stun:stun.voys.nl:3478"
+        ])
+        "utls-imitate=hellorandomizedalpn"
+      ];
+      ClientTransportPlugin = "snowflake exec ${pkgs.snowflake}/bin/client";
+      UseBridges = 1;
+      ReachableAddresses = [ "*:80" "*:443" ];
+    };
   };
 }
