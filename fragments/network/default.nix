1cf3e5ad8360c1273ec6107a832f5013bdb4cb74
Igor Sharonov <igsha@users.noreply.github.com>
2024-01-12 21:27:06 +0300

    network: Add ministry of digital development certificate

--- fragments/network/default.nix
+++ fragments/network/default.nix
@@ -25,4 +25,15 @@ _:
     "systemd-ask-password-wall.path"
     "systemd-ask-password-wall.service"
   ];
+
+  security.pki.certificateFiles = [
+    (builtins.fetchurl {
+      url = "https://gu-st.ru/content/lending/russian_trusted_root_ca_pem.crt";
+      sha256 = sha256:0135zid0166n0rwymb38kd5zrd117nfcs6pqq2y2brg8lvz46slk;
+    })
+    (builtins.fetchurl {
+      url = "https://gu-st.ru/content/lending/russian_trusted_sub_ca_pem.crt";
+      sha256 = sha256:19jffjrawgbpdlivdvpzy7kcqbyl115rixs86vpjjkvp6sgmibph;
+    })
+  ];
 }
