d325bce71320c21b44d25553925a9251777d064f
Igor Sharonov <igsha@users.noreply.github.com>
2024-03-11 17:32:44 +0300

    neovim: Fix tags plugin

--- fragments/programs/neovim/home-config/nvim/.config/nvim/lua/plugin/tags.lua
+++ fragments/programs/neovim/home-config/nvim/.config/nvim/lua/plugin/tags.lua
@@ -6,9 +6,9 @@ local function MakeTagsInGitRootDir()
     local tagfile = rootdir .. '/.git/tags'
     local excluded = [[-type d \( -path "*/.git" -o -path "*/build" \) -prune]]
     local pattern = [[\( -name "*.[ch]" -o -name "*.[ch]pp" -o -name "*.[ch]xx" -o -name "*.cc" -o -name "*.hh" \)]]
-    local filelist = vm.fn.systemlist('find ' .. rootdir .. ' ' .. excluded .. ' -o ' .. pattern)
+    local filelist = vim.fn.systemlist('find ' .. rootdir .. ' ' .. excluded .. ' -o ' .. pattern)
     pipe = assert(io.popen('ctags -L - --c++-kinds=+p --fields=+iaS --extras=+q -f ' .. tagfile, 'w'))
-    for line in filelist do
+    for number,line in pairs(filelist) do
         pipe:write(line, '\n')
     end
     pipe:close()
