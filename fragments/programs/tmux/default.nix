beb5d1f83a7bd8cd32d9d757541932ecdf493ae0
Igor Sharonov <igsha@users.noreply.github.com>
2024-04-30 14:30:15 +0300

    tmux: Use simple configuration + mycollection

--- fragments/programs/tmux/default.nix
+++ fragments/programs/tmux/default.nix
@@ -1,51 +1,34 @@
 { config, pkgs, ... }:
 
 let
-  loadavg = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "loadavg";
-    rtpFilePath = "tmux-loadavg.tmux";
-    version = "unstable-2018-05-30";
+  mycollection = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "mycollection";
+    version = "unstable-2024-04-30";
     src = pkgs.fetchFromGitHub {
-      owner = "jamesoff";
-      repo = "tmux-loadavg";
-      rev = "91319eff74ee677efb77c882dcc8e3b8780dc3a2";
-      hash = "sha256-JW/O/bVryDILlFCnWYRC+B7nrCIsGYEJDozzu0odX+U=";
+      owner = "igsha";
+      repo = "tmux-mycollection";
+      rev = "b3528484d99aa41d9aafd371ede4147553e5123f";
+      hash = "sha256-oCbOwATpLBdyb0pWgEWn3pdcTgHv3dvlTH/8kU22QWA=";
     };
   };
 
 in {
-  programs.tmux = {
-    enable = true;
-    clock24 = true;
-    terminal = "screen-256color";
-    keyMode = "vi";
-    shortcut = "a";
-    baseIndex = 1;
-    escapeTime = 0;
-    historyLimit = 50000;
-    plugins = with pkgs.tmuxPlugins; [
-      prefix-highlight
-      sidebar
-      urlview
-      yank
-      pain-control
-      logging
-      open
-      copycat
-      sysstat
-      net-speed
-      loadavg
-    ];
-    extraConfigBeforePlugins = let
-      battery = "[#(cat /sys/class/power_supply/BAT0/capacity)]";
-      batteryline = if config.custom-args.battery or false then "${battery} " else "";
-      sysstat = "#{sysstat_mem}[#{sysstat_swap}]";
-      datetime = "%a %Y-%m-%d %H:%M";
-      la = "#{load_short}";
-      statusline = "#{prefix_highlight} ${batteryline}${la} ${sysstat} #{net_speed} | ${datetime} | #H";
-    in ''
-      set -g status-right '${statusline}'
-    '';
+  environment = {
+    systemPackages = [ pkgs.tmux ];
+    etc.tmux.source = pkgs.symlinkJoin {
+      name = "tmux-plugins";
+      paths = with pkgs.tmuxPlugins; [
+        prefix-highlight
+        sidebar
+        urlview
+        yank
+        pain-control
+        logging
+        open
+        copycat
+        mycollection
+      ];
+    };
   };
 
   home-config.tmux = {
