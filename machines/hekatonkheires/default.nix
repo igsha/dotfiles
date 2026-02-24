1ccfba0f91e1ce5dc64466a97cf8775886888263
Igor Sharonov <igsha@users.noreply.github.com>
2026-02-24 09:19:16 +0300

    hekatonkheires: Add postgres db

--- machines/hekatonkheires/default.nix
+++ machines/hekatonkheires/default.nix
@@ -56,6 +56,20 @@
       HandleLidSwitchDocked = "ignore";
       HandleLidSwitchExternalPower = "ignore";
     };
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "mydatabase" ];
+      enableTCPIP = true;
+      settings.port = 5432;
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database DBuser origin-address auth-method
+        local all      all     trust
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host all       all     ::1/128        trust
+      '';
+    };
   };
 
   boot = {
